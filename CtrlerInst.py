9f472f56643bc29c281dadba78c49fa5331d3efc
Xiaokang Wang <xiaokangwang@outlook.com>
2014-01-27 12:05:11 +0800

    Config basic

---
+++ CtrlerInst.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+
+WorkMode={
+'WorkNode', #Accetpt Control
+'ControlConsole' #Connect to Server Accept Control
+}
+
+class KKUniCtrlConsole_CtrlerInst(object):
+    """The Control Instance of KKUniCtrlConsole."""
+
+    InstConf={}
+
+    def __init__(self):
+        super(KKUniCtrlConsole_CtrlerInst, self).__init__()
+
+    def SetConfig(self,key,val):
+        self.InstConf[key]=val
+
+    def GetConfig(self,key):
+        if key in self.InstConf:
+            return self.InstConf[key]
+        else:
+            raise Exception("Not exist.")
+
+    def LoadConfig(self,confDict):
+        self.InstConf.update(confDict)
+
+    def InitConfig(self,Mode):
+        
+        if Mode in WorkMode:
+
+            self.InstConf={
+            "Mode"=Mode
+            }
+
+        if Mode == "WorkNode":
+            self.InstConf['CtrleeProject']={}
+            self.InstConf['CtrleeInstance']={}
+            self.InstConf['Controlers']=[]
+            self.InstConf['ControlerInterface']=[]
+
+        if Mode == "ControlConsole":
+            self.InstConf['CtrleeProjects']=[]
+            self.InstConf['Controler']={}
+
+    def ExportConfig(self):
+        return self.InstConf
+
+
+
