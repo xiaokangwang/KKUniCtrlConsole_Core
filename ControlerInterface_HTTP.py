f8653a9ff9444b6760f191ff124aaf2487b264d1
Xiaokang Wang <xiaokangwang@outlook.com>
2014-01-27 12:57:03 +0800

    ControlerInterface_HTTP common init

---
+++ ControlerInterface_HTTP.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import uuid
+"""ControlerInterface:HTTP Progresser"""
+
+WorkMode={
+'WorkNode', #Accetpt Control
+'ControlConsole' #Connect to Server Accept Control
+}
+
+class ControlerInterface_AMQP(object):
+	"""The HTTP Controler interface Instance"""
+	InstConf={}
+	Runtimevar={}
+	CtrlerRuntime={}
+	def __init__(self):
+		super(ControlerInterface_AMQP, self).__init__()
+		
+	def SetConfig(self,key,val):
+        self.InstConf[key]=val
+
+    def GetConfig(self,key):
+        if key in self.InstConf:
+            return self.InstConf[key]
+        else:
+            raise Exception("Not exist.")
+
+    def LoadConfig(self,confDict):
+        self.InstConf.update(confDict)
+
+    def InitConfig(self,Mode):
+        
+        if Mode in WorkMode:
+
+            self.InstConf={
+            "Mode"=Mode
+            }
+
+        self.InstConf['HTTP_URL']=''
+
+        if Mode == "WorkNode":
+        	pass
+            
+        if Mode == "ControlConsole":
+        	pass
+
+
+    def ExportConfig(self):
+        return self.InstConf
+
+    def InitRuntime(self,CtrlerRuntime):
+        self.Runtimevar['uuid']=str(uuid.uuid4())
+        self.CtrlerRuntime=CtrlerRuntime
+
+    
+
